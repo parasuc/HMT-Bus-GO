925bc98479275cdf284b4bd893d3e644832dff77
CRH380A-2722 <609657831@qq.com>
2017-03-07 00:17:03 +0800

    Add database structure SQL dump file

---
+++ sql/struct.sql
@@ -0,0 +1,38 @@
+--
+--	SCAU Bus GO -- 华农校巴查询程序
+--
+--	@note 数据库结构 v1.0
+--
+--	@author CRH380A-2722 <609657831@qq.com>
+--	@license MIT
+--	@copyright 2016-2017 CRH380A-2722
+--
+
+CREATE TABLE IF NOT EXISTS bus_line (
+	line_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	line_name VARCHAR(50) NOT NULL,
+	first_bus CHAR(5) NOT NULL,
+	last_bus CHAR(5) NOT NULL,
+	line_start INT UNSIGNED NOT NULL,
+	line_end INT UNSIGNED NOT NULL,
+	PRIMARY KEY (line_id),
+	INDEX (line_start),
+	INDEX (line_end)
+) ENGINE = MyISAM DEFAULT CHARSET = 'utf8';
+
+CREATE TABLE IF NOT EXISTS bus_stop (
+	stop_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	stop_name VARCHAR(50) NOT NULL,
+	stop_long VARCHAR(50) DEFAULT NULL, -- 经度
+	stop_lat VARCHAR(50) DEFAULT NULL, -- 纬度
+	PRIMARY KEY (stop_id)
+) ENGINE = MyISAM DEFAULT CHARSET = 'utf8';
+
+CREATE TABLE IF NOT EXISTS bus_relationship (
+	line_id INT UNSIGNED NOT NULL,
+	stop_id INT UNSIGNED NOT NULL,
+	stop_sort INT UNSIGNED NOT NULL,
+	INDEX (line_id),
+	INDEX (stop_id),
+	INDEX (stop_sort)
+) ENGINE = MyISAM DEFAULT CHARSET = 'utf8';
