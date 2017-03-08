cc7777483c913952916feb2c7d422611f587c278
CRH380A-2722 <609657831@qq.com>
2017-03-08 17:25:58 +0800

    Add school bus stop data

---
+++ data/stop.sql
@@ -0,0 +1,31 @@
+--
+--	SCAU Bus GO -- 华农校巴查询程序
+--
+--	@note 校巴站点数据
+--
+--	@author CRH380A-2722 <609657831@qq.com>
+--	@license MIT
+--	@copyright 2016-2017 CRH380A-2722
+--
+
+INSERT INTO bus_stop (stop_id, stop_name, stop_lng, stop_lat) VALUES
+(1, '西园', NULL, NULL),
+(2, '工程学院', NULL, NULL),
+(3, '新学活', NULL, NULL),
+(4, '第三教学楼', NULL, NULL),
+(5, '艺术学院', NULL, NULL),
+(6, '三角市', NULL, NULL),
+(7, '湿地公园', NULL, NULL),
+(8, '经管学院', NULL, NULL),
+(9, '第四教学楼', NULL, NULL),
+(10, '五山学生公寓', NULL, NULL),
+(11, '公寓东', NULL, NULL),
+(12, '荷园', NULL, NULL),
+(13, '农学院', NULL, NULL),
+(14, '嵩山42栋', NULL, NULL),
+(15, '正门（地铁站）', NULL, NULL),
+(16, '生命科学院', NULL, NULL),
+(17, '旧保卫处', NULL, NULL),
+(18, '嵩山旧197', NULL, NULL),
+(19, '校医院东门', NULL, NULL),
+(20, '茶山广场', NULL, NULL);
