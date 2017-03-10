add4fb23e7de724481ae9ca052fae442562986d0
CRH380A-2722 <609657831@qq.com>
2017-03-11 01:44:51 +0800

    Add .page-header

--- templates/template.LineInfo.php
+++ templates/template.LineInfo.php
@@ -1,3 +1,7 @@
+				<div class="page-header">
+					<h3><span class="fa fa-bus"></span> 线路详情</h3>
+				</div>
+
 				<div class="panel panel-default">
 					<div class="panel-heading">
 						<h3 class="panel-title"><span class="fa fa-bus"></span> 线路信息</h3>
@@ -10,11 +14,11 @@
 
 				<div class="panel panel-success">
 					<div class="panel-heading">
-						<h3 class="panel-title"><span class="fa fa-road"></span> 途径站点</h3>
+						<h3 class="panel-title"><span class="fa fa-road"></span> 途径站点（顺序从上到下）</h3>
 					</div>
 					<div class="list-group">
 					<?php for ($i = 0; $i < $this->data['totalStops']; $i++): ?>
-						<a href="./index.php?mod=stopinfo&id=<?php print $this->data['stops'][$i]['stop_id']; ?>" class="list-group-item"><span class="badge"><?php print $this->data['stops'][$i]['stop_sort']; ?></span> <span class="fa fa-map-marker"></span> <?php print $this->data['stops'][$i]['stop_name']; ?></a>
+						<a href="./index.php?mod=stopinfo&id=<?php print $this->data['stops'][$i]['stop_id']; ?>" class="list-group-item"><span class="fa fa-map-marker"></span> <?php print $this->data['stops'][$i]['stop_name']; ?></a>
 					<?php endfor; ?>
 					</div>
 				</div>
