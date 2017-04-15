1e8ce0794ebfeed1ee4574d2e42fdd6cbb459a8d
CRH380A-2722 <609657831@qq.com>
2017-04-16 03:50:43 +0800

    采用栅格布局

--- templates/template.LineInfo.php
+++ templates/template.LineInfo.php
@@ -13,27 +13,33 @@
 					</div>
 				</div>
 
-				<div class="panel panel-success">
-					<div class="panel-heading">
-						<h3 class="panel-title"><span class="fa fa-road"></span> 途经站点（顺序从上到下）</h3>
+				<div class="row">
+					<div class="col-sm-8">
+						<div class="panel panel-success">
+							<div class="panel-heading">
+								<h3 class="panel-title"><span class="fa fa-road"></span> 途经站点（顺序从上到下）</h3>
+							</div>
+							<div class="list-group" id="stop-list">
+							<?php for ($i = 0; $i < $this->data['totalStops']; $i++): ?>
+								<a href="./index.php?mod=stopinfo&id=<?php print $this->data['stops'][$i]['stop_id']; ?>" class="list-group-item">
+								<?php if ($this->data['stops'][$i]['have_bus']): ?>
+									<span class="badge"><span class="fa fa-bus"></span> <?php print $this->data['stops'][$i]['have_bus']; ?></span> 
+								<?php endif; ?>
+									<span class="fa fa-map-marker"></span> <?php print $this->data['stops'][$i]['stop_name']; ?></a>
+							<?php endfor; ?>
+							</div>
+						</div>
 					</div>
-					<div class="list-group" id="stop-list">
-					<?php for ($i = 0; $i < $this->data['totalStops']; $i++): ?>
-						<a href="./index.php?mod=stopinfo&id=<?php print $this->data['stops'][$i]['stop_id']; ?>" class="list-group-item">
-						<?php if ($this->data['stops'][$i]['have_bus']): ?>
-							<span class="badge"><span class="fa fa-bus"></span> <?php print $this->data['stops'][$i]['have_bus']; ?></span> 
-						<?php endif; ?>
-							<span class="fa fa-map-marker"></span> <?php print $this->data['stops'][$i]['stop_name']; ?></a>
-					<?php endfor; ?>
-					</div>
-				</div>
 
-				<div class="panel panel-info">
-					<div class="panel-heading">
-						<h3 class="panel-title"><span class="fa fa-cog"></span> 操作</h3>
-					</div>
-					<div class="panel-body" data-toggle="buttons">
-						<button id="stop-refresh-btn" type="button" class="btn btn-primary"><span class="fa fa-refresh"></span></span> 刷新数据</button> 
-						<label class="btn btn-success" id="stop-auto-refresh-btn"><input id="auto-refresh-flag" type="checkbox" autocomplete="off"><span id="auto-refresh-logo" class="fa fa-refresh"></span> 自动刷新 (<span id="status">OFF</span>)</label>
+					<div class="col-sm-4">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+								<h3 class="panel-title"><span class="fa fa-cog"></span> 操作</h3>
+							</div>
+							<div class="panel-body" data-toggle="buttons">
+								<button id="stop-refresh-btn" type="button" class="btn btn-primary"><span class="fa fa-refresh"></span></span> 刷新数据</button> 
+								<label class="btn btn-success" id="stop-auto-refresh-btn"><input id="auto-refresh-flag" type="checkbox" autocomplete="off"><span id="auto-refresh-logo" class="fa fa-refresh"></span> 自动刷新 (<span id="status">OFF</span>)</label>
+							</div>
+						</div>
 					</div>
 				</div>
--- templates/template.StopIndex.php
+++ templates/template.StopIndex.php
@@ -2,35 +2,41 @@
 					<h3><span class="fa fa-bus"></span> 校巴站点查询</h3>
 				</div>
 
-				<div class="panel panel-primary">
-					<div class="panel-heading">
-						<h3 class="panel-title"><span class="fa fa-search"></span> 站点检索</h3>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								<h3 class="panel-title"><span class="fa fa-search"></span> 站点检索</h3>
+							</div>
+							<div class="panel-body">
+								<form id="stopSearch" action="./index.php?mod=stopsearch" method="get">
+									<p>
+										<div class="input-group">
+											<span class="input-group-addon">站点名称</span>
+											<input type="text" id="stopName" class="form-control" name="name" placeholder="请键入车站名称..." required>
+										</div>
+									</p>
+									<p>
+										<button type="submit" id="submitBtn" class="btn btn-primary form-control"><span class="fa fa-search"></span> 查询</button>
+									</p>
+								</form>
+							</div>
+						</div>
 					</div>
-					<div class="panel-body">
-						<form id="stopSearch" action="./index.php?mod=stopsearch" method="get">
-							<p>
-								<div class="input-group">
-									<span class="input-group-addon">站点名称</span>
-									<input type="text" id="stopName" class="form-control" name="name" placeholder="请键入车站名称..." required>
-								</div>
-							</p>
-							<p>
-								<button type="submit" id="submitBtn" class="btn btn-primary form-control"><span class="fa fa-search"></span> 查询</button>
-							</p>
-						</form>
-					</div>
-				</div>
 
-				<div class="panel panel-primary">
-					<div class="panel-heading">
-						<h3 class="panel-title"><span class="fa fa-map-marker"></span> 站点列表</h3>
-					</div>
-					<div class="panel-body">
-						亦可点击下面的站点查看详细信息：
-					</div>
-					<div class="list-group">
-					<?php for ($i = 0; $i < $this->data['total']; $i++): ?>
-						<a href="./index.php?mod=stopinfo&id=<?php print $this->data['rows'][$i]['stop_id']; ?>" class="list-group-item"><span class="fa fa-map-marker"></span> <?php print $this->data['rows'][$i]['stop_name']; ?></a>
-					<?php endfor; ?>
+					<div class="col-sm-8">
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								<h3 class="panel-title"><span class="fa fa-map-marker"></span> 站点列表</h3>
+							</div>
+							<div class="panel-body">
+								亦可点击下面的站点查看详细信息：
+							</div>
+							<div class="list-group">
+							<?php for ($i = 0; $i < $this->data['total']; $i++): ?>
+								<a href="./index.php?mod=stopinfo&id=<?php print $this->data['rows'][$i]['stop_id']; ?>" class="list-group-item"><span class="fa fa-map-marker"></span> <?php print $this->data['rows'][$i]['stop_name']; ?></a>
+							<?php endfor; ?>
+							</div>
+						</div>
 					</div>
 				</div>
