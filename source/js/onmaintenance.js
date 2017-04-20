6f1683d2c0cb7ddc21fc63cdeab22ec74d3d818b
CRH380A-2722 <609657831@qq.com>
2017-04-20 23:13:20 +0800

    系统维护页用的JavaScript

---
+++ source/js/onmaintenance.js
@@ -0,0 +1,17 @@
+/*!
+ *	HMT Bus GO -- 华农校巴查询程序
+ *
+ *	@author CRH380A-2722 <609657831@qq.com>
+ *	@copyright 2016-2017 CRH380A-2722
+ *	@license MIT
+ *	@note 首页JavaScript
+ */
+
+(function($) {
+	$(window).load(function() {
+		$("#mask").fadeOut("slow");
+		setTimeout(function() {
+			$("#container").fadeIn("slow");
+		}, 1000);
+	});
+})(jQuery);
