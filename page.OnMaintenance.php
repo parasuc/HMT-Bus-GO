7a67d7cd09af868a436b43e323e8055e8e1eb451
CRH380A-2722 <609657831@qq.com>
2017-04-20 23:12:56 +0800

    新增系统维护页

---
+++ page.OnMaintenance.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *	HMT Bus GO -- 华农校巴查询程序
+ *
+ *	@author CRH380A-2722 <609657831@qq.com>
+ *	@copyright 2016-2017 CRH380A-2722
+ *	@license MIT
+ *	@note 维护页面
+ */
+
+$this->Template->setPageTitle('站点维护中');
+
+$this->Template->need('OnMaintenance');
