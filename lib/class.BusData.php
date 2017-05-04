974d91409a1d07b1af5ef1ab324ad94fd305f03d
CRH380A-2722 <609657831@qq.com>
2017-05-04 22:17:03 +0800

    添加获取线路路径的相关方法

--- lib/class.BusData.php
+++ lib/class.BusData.php
@@ -21,7 +21,7 @@ class BusData extends DB {
 		parent::query("SELECT line_id, line_name, line_start, line_end FROM bus_line ORDER BY line_id ASC;");
 		$rows = array();
 		$i = 0;
-		while( $row = parent::fetchArray() ) {
+		while ( $row = parent::fetchArray() ) {
 			$rows[$i] = $row;
 			$i++;
 		}
@@ -42,7 +42,7 @@ class BusData extends DB {
 		parent::query("SELECT * FROM bus_line WHERE line_id = {$lineId} LIMIT 1;");
 		$rows = array();
 		$i = 0;
-		while( $row = parent::fetchArray() ) {
+		while ( $row = parent::fetchArray() ) {
 			$rows[$i] = $row;
 			$i++;
 		}
@@ -63,7 +63,7 @@ class BusData extends DB {
 		parent::query("SELECT s.stop_id, s.stop_name, r.stop_sort FROM bus_stop AS s LEFT JOIN bus_relationship AS r USING (stop_id) LEFT JOIN bus_line AS l USING (line_id) WHERE line_id = {$lineId} ORDER BY stop_sort ASC;");
 		$rows = array();
 		$i = 0;
-		while( $row = parent::fetchArray() ) {
+		while ( $row = parent::fetchArray() ) {
 			$rows[$i] = $row;
 			$i++;
 		}
@@ -84,7 +84,7 @@ class BusData extends DB {
 		parent::query("SELECT * FROM bus_stop WHERE stop_id = {$stopId} LIMIT 1;");
 		$rows = array();
 		$i = 0;
-		while( $row = parent::fetchArray() ) {
+		while ( $row = parent::fetchArray() ) {
 			$rows[$i] = $row;
 			$i++;
 		}
@@ -103,7 +103,7 @@ class BusData extends DB {
 		parent::query("SELECT stop_id, stop_name FROM bus_stop WHERE stop_name = '{$stopName}' ORDER BY stop_id ASC;");
 		$rows = array();
 		$i = 0;
-		while( $row = parent::fetchArray() ) {
+		while ( $row = parent::fetchArray() ) {
 			$rows[$i] = $row;
 			$i++;
 		}
@@ -120,7 +120,7 @@ class BusData extends DB {
 		parent::query("SELECT stop_id, stop_name FROM bus_stop;");
 		$rows = array();
 		$i = 0;
-		while( $row = parent::fetchArray() ) {
+		while ( $row = parent::fetchArray() ) {
 			$rows[$i] = $row;
 			$i++;
 		}
@@ -138,7 +138,7 @@ class BusData extends DB {
 		parent::query("SELECT stop_name FROM bus_stop;");
 		$rows = array();
 		$i = 0;
-		while( $row = parent::fetchArray() ) {
+		while ( $row = parent::fetchArray() ) {
 			$rows[$i] = $row;
 			$i++;
 		}
@@ -160,7 +160,7 @@ class BusData extends DB {
 		parent::query("SELECT l.line_id, l.line_name, l.line_start, l.line_end FROM bus_stop AS s LEFT JOIN bus_relationship as r USING (stop_id) LEFT JOIN bus_line AS l USING (line_id) WHERE stop_id = {$stopId} ORDER BY line_id ASC;");
 		$rows = array();
 		$i = 0;
-		while( $row = parent::fetchArray() ) {
+		while ( $row = parent::fetchArray() ) {
 			$rows[$i] = $row;
 			$i++;
 		}
@@ -184,7 +184,7 @@ class BusData extends DB {
 		parent::query(sprintf("SELECT t.* FROM bus_timetable AS t LEFT JOIN bus_line AS l USING (line_id) WHERE line_id = %d AND table_type = %d ORDER BY table_id ASC;", $lineId, $type));
 		$rows = array();
 		$i = 0;
-		while( $row = parent::fetchArray() ) {
+		while ( $row = parent::fetchArray() ) {
 			$rows[$i] = $row;
 			$i++;
 		}
@@ -201,10 +201,63 @@ class BusData extends DB {
 		parent::query("SELECT * FROM bus_stop ORDER BY stop_id ASC;");
 		$rows = array();
 		$i = 0;
-		while( $row = parent::fetchArray() ) {
+		while ( $row = parent::fetchArray() ) {
 			$rows[$i] = array(
 				'title' => $row['stop_name'],
 				'position' => array($row['stop_lng'], $row['stop_lat'])
+			);
+			$i++;
+		}
+		return $rows;
+	}
+
+	/**
+	 *	通过线路ID获取该线路的路径
+	 *
+	 *	@param int $lineId [线路ID]
+	 *	@return array
+	 */
+
+	public function getPolylineByLineId($lineId) {
+		if (!Verifier::isNumber($lineId)) {
+			throw new Error('传入线路ID有误');
+		}
+		parent::query(sprintf("SELECT * FROM bus_polyline LEFT JOIN bus_line USING (line_id) WHERE line_id = %d LIMIT 1;", $lineId));
+		$rows = array();
+		$i = 0;
+		while ( $row = parent::fetchArray() ) {
+			$rows[$i] = array(
+				'line_id' => $row['line_id'],
+				'line_name' => $row['line_name'],
+				'line_start' => $row['line_start'],
+				'line_end' => $row['line_end'],
+				'poly_color' => $row['poly_color'],
+				'poly_path' => json_decode($row['poly_path'], true)
+			);
+			$i++;
+		}
+		return $rows;
+	}
+
+	/**
+	 *	通过线路ID获取该线路的路径
+	 *
+	 *	@param int $lineId [线路ID]
+	 *	@return array
+	 */
+
+	public function getPolyline() {
+		parent::query("SELECT * FROM bus_polyline LEFT JOIN bus_line USING (line_id) ORDER BY line_id ASC;");
+		$rows = array();
+		$i = 0;
+		while ( $row = parent::fetchArray() ) {
+			$rows[$i] = array(
+				'line_id' => $row['line_id'],
+				'line_name' => $row['line_name'],
+				'line_start' => $row['line_start'],
+				'line_end' => $row['line_end'],
+				'poly_color' => $row['poly_color'],
+				'poly_path' => json_decode($row['poly_path'], true)
 			);
 			$i++;
 		}
