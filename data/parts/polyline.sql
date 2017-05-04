a3696d58957e2d67aae2659c14051f81581b0ab0
CRH380A-2722 <609657831@qq.com>
2017-05-04 14:07:12 +0800

    //WIP

---
+++ data/parts/polyline.sql
@@ -0,0 +1,18 @@
+--
+--	HMT Bus GO -- 华农校巴查询程序
+--
+--	@note 校巴线路路径信息 #201705040010
+--
+--	@author CRH380A-2722 <609657831@qq.com>
+--	@license MIT
+--	@copyright 2016-2017 CRH380A-2722
+--
+
+INSERT INTO bus_polyline (poly_id, line_id, poly_color, poly_path) VALUES
+(0, 1, '', ''),
+(0, 2, '', ''),
+(0, 3, '', ''),
+(0, 4, '', ''),
+(0, 5, '', ''),
+(0, 6, '', ''),
+(0, 7, '', '');
